9ef58f84c8eeffaf486b7265efd85db72caf8e05
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-26 11:26:49 -0800

    added database

---
+++ database/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"email" TEXT NOT NULL,
+	"hashedpassword" TEXT NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "applications" (
+	"applicationId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"who" TEXT NOT NULL,
+	"what" TEXT NOT NULL,
+	"when" TEXT NOT NULL,
+	"where" TEXT NOT NULL,
+	"why" TEXT NOT NULL,
+	"how" TEXT NOT NULL,
+	CONSTRAINT "applications_pk" PRIMARY KEY ("applicationId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "applications" ADD CONSTRAINT "applications_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
