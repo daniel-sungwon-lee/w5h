06da19bd40cd3af889c91940f5143cbb486a1051
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-02 16:47:11 -0800

    added build directory

--- server/index.js
+++ server/index.js
@@ -198,11 +198,11 @@ app.patch('/api/application/:userId/:applicationId', (req, res, next) => {
 //for Heroku deployment
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
