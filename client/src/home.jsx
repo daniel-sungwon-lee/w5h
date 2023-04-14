0f7206338391a05e179ae09b1ed4e20f70c861bb
Daniel Sungwon Lee <danlee333@gmail.com>
2023-04-14 09:38:44 -0700

    bg color to list

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -28,7 +28,8 @@ const useStyles = makeStyles({
   listItemCard: {
     boxShadow: "0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)",
     margin: "3rem 0",
-    borderRadius: "1.5rem"
+    borderRadius: "1.5rem",
+    background: "rgb(213, 247, 198)"
   },
   popup: {
     borderRadius: "3rem"
